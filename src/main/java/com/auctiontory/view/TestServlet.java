826d35be8a80dd85c274b1e748e34970545dda9a
MuhammedMahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2018-04-05 18:16:30 +0200

    fixed dependency issue.

--- src/main/java/com/auctiontory/view/TestServlet.java
+++ src/main/java/com/auctiontory/view/TestServlet.java
@@ -1,5 +1,6 @@
 package com.auctiontory.view;
 
+import com.auctiontory.controller.UserController;
 import com.auctiontory.controller.impl.UserControllerImpl;
 import com.auctiontory.model.entity.User;
 import com.auctiontory.model.entity.UserType;
@@ -17,7 +18,7 @@ import java.util.List;
 public class TestServlet extends HttpServlet {
 
     @Inject
-    UserControllerImpl userEjbBean;
+    UserController userControllerImpl;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -25,16 +26,16 @@ public class TestServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = new User("ABC", "12345", "m@YYY.com", UserType.INDIVIDUAL);
-        userEjbBean.save(user);
+        userControllerImpl.save(user);
         try {
-            userEjbBean.login(user.getUserName(), user.getPassword());
+            userControllerImpl.login(user.getUserName(), user.getPassword());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        User anotherRef = userEjbBean.get(user.getId());
+        User anotherRef = userControllerImpl.get(user.getId());
         anotherRef.setEmail("aaaa@mail.com");
-        userEjbBean.update(anotherRef);
-        userEjbBean.delete(anotherRef);
-        List<User> users = userEjbBean.loadAll();
+        userControllerImpl.update(anotherRef);
+        userControllerImpl.delete(anotherRef);
+        List<User> users = userControllerImpl.loadAll();
     }
 }
